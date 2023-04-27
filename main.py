850b3b3fd5787a84af90852d98ae95273aa043bf
DESKTOP-OR0EAIA\Escal <escalonaf12@gmail.com>
2023-04-27 01:58:02 -0400

    changing csv format

--- main.py
+++ main.py
@@ -3,50 +3,70 @@ import socketserver
 import json
 import threading
 import time
+import csv
 
 invitados = {}  
 last_post_data = {}  
-detected_guests = set()  # Conjunto para almacenar los nombres de los invitados detectados
+detected_guests = set()  
 EXPIRATION_TIME = 5
-
-
-with open("invitados.txt", "r") as file:
-    for line in file:
-        name, id_hex = line.strip().split(",")
-        invitados[id_hex] = name
+with open("invitados.csv", "r") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        if len(line) == 5:
+            id_hex, nombre, apellido, organizacion, estado = line
+            invitados[id_hex] = {
+                "nombre": nombre,
+                "apellido": apellido,
+                "organizacion": organizacion,
+                "estado": estado
+            }
+        else:
+            print(f"Error: La siguiente línea tiene un número incorrecto de campos: {line}")
 
 def find_invitado(id_hex):
     return invitados.get(id_hex, None)
 
 def remove_expired_invitados():
-    
     while True:
         current_time = time.time()
         non_expired_invitados = {invitado: last_post_time for invitado, last_post_time in last_post_data.items() if current_time - last_post_time <= EXPIRATION_TIME}
-
-        # Reemplazar el diccionario last_post_data con el diccionario de invitados no expirados
         last_post_data.update(non_expired_invitados)
-
         time.sleep(EXPIRATION_TIME)
 
+def update_invitado_status_in_csv(id_hex, new_status):
+    with open("invitados.csv", "r") as file:
+        lines = file.readlines()
+
+    with open("invitados.csv", "w") as file:
+        for line in lines:
+            if line.startswith(id_hex):
+                line = ','.join(line.split(',')[:-1]) + f",{new_status}\n"
+            file.write(line)
 
 class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
-    
     def do_GET(self):
         if self.path == "/api/guests":
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
 
-            # Filtrar invitados no expirados
             current_time = time.time()
             non_expired_invitados = {invitado: last_post_time for invitado, last_post_time in last_post_data.items() if current_time - last_post_time <= EXPIRATION_TIME}
-            
-            response_data = json.dumps(list(non_expired_invitados.keys()))
-            self.wfile.write(response_data.encode("utf-8"))
+
+            response_data = []
+            for invitado_id in non_expired_invitados.keys():
+                invitado = find_invitado(invitado_id)
+                if invitado and invitado["estado"] == "A":
+                    response_data.append({
+                        "nombre": invitado["nombre"],
+                        "apellido": invitado["apellido"],
+                        "organizacion": invitado["organizacion"]
+                    })
+
+            self.wfile.write(json.dumps(response_data).encode("utf-8"))
         else:
             super().do_GET()
-        
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
@@ -68,8 +88,6 @@ class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Received POST data")
 
-            
-
     def end_headers(self):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
@@ -79,15 +97,16 @@ class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
     def process_post_data(self, item):
         id_hex = item['data'].get('idHex', None)
         if id_hex:
-            # Busca al invitado aquí en lugar de hacerlo fuera de este método
             invitado = find_invitado(id_hex)
             if invitado:
-                last_post_data[invitado] = time.time()
+                last_post_data[id_hex] = time.time()
                 print(f"Invitado: {invitado}, idHex: {id_hex}")
+                if invitado["estado"] == "F":
+                    invitado["estado"] = "A"
+                    update_invitado_status_in_csv(id_hex, "A")
 
 
 if __name__ == "__main__":
-  
     t = threading.Thread(target=remove_expired_invitados, daemon=True)
     t.start()
 
@@ -96,3 +115,4 @@ if __name__ == "__main__":
     httpd = socketserver.TCPServer(("0.0.0.0", PORT), handler)
     print(f"Serving on port {PORT}")
     httpd.serve_forever()
+
